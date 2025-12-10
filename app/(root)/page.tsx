5def7df1a774de25bdaa7a8eba1a391d26dff878
amaan025 <amaan23455@gmail.com>
2025-12-10 18:58:39 +0530

    implemented start up card template

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
 import {SearchParams} from "next/dist/server/request/search-params";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}){
 
+
+
     const query = (await searchParams).query;
 
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: {_id: 1, name: "Amaan Ahmad"},
+        _id: 1,
+        description: 'This is a description',
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNu5Y7TGPb_e2NEy4KhUaWwQxF3RRtxb80YQ&s",
+        category: "Artificial Intelligence",
+        title: "AI"
+    }]
   return (
       <>
           <section className="pink_container pattern">
@@ -14,6 +27,22 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
                   Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
               </p>
               <SearchForm query={query}/>
+          </section>
+
+          <section className="section-container">
+              <p className="text-30-semibold">
+                  {query ? `Search results for ${query}` : 'All Startups'}
+              </p>
+
+              <ul className="mt-7 card_grid">
+                  {posts?. length > 0 ? (
+                      posts.map((posts: StartupCardType, index: number) => (
+                          <StartupCard key={posts?._id} posts={posts}/>
+                      ))
+                  ): (
+                      <p className="no-results">No startups found</p>
+                  )}
+              </ul>
           </section>
 
       </>
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*'
+            }
+        ]
+    }
   /* config options here */
 };
 
